b358f46294924dc3cf4726516399a4b5a2afe415
ahadakash <sadhinakash181@gmail.com>
2023-09-23 22:12:41 +0600

    5

---
+++ src/components/Products/Products.js
@@ -0,0 +1,29 @@
+import React, { useState } from 'react';
+import './Products.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCartShopping } from '@fortawesome/free-solid-svg-icons';
+
+const Products = (props) => {
+    // const [product, setProduct] = useState();
+    // const products = props.product.slice(0, 10);
+    // console.log(props.product);
+    const { name, img, price, stock, seller } = props.product;
+
+    return (
+        <div className='product-container'>
+            <div>
+                <img src={img} alt="" />
+            </div>
+            <div className="div">
+                <h3 style={{ color: 'blue', fontWeight: '400' }}>{name}</h3>
+                <p>by: {seller}</p>
+                <p>${price}</p>
+                <p>only {stock} left in stock - order soon</p>
+                <button className='cart-button' onClick={props.handleClick}><FontAwesomeIcon icon={faCartShopping} /> add to cart</button>
+            </div>
+
+        </div >
+    );
+};
+
+export default Products;
--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -1,15 +1,25 @@
 import React from 'react';
 import './Shop.css';
-import fakedata from '../../fakeData/products.JSON';
+import fakedata from '../../fakeData/products.json';
+import Products from '../Products/Products';
 
 const Shop = () => {
-
+    const handleClick = () => {
+        console.log("Clicked");
+    }
     return (
-        <div className='header'>
-            <h1>This is Shop</h1>
-            {
-                fakedata.map((data) => <h2>Hello</h2>)
-            }
+        <div className='shop-container'>
+            <div className="product">
+                {
+                    fakedata.map((data) =>
+                        <div>
+                            <Products product={data} handleClick={handleClick}></Products>
+                        </div>)
+                }
+            </div>
+            <div className="cart">
+                <h1>This is cart</h1>
+            </div>
         </div>
     );
 };
